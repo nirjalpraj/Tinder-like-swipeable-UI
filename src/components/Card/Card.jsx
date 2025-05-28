f3770d8bafe94efb215464d53e849b1126cf3f0d
Nirjal Prajapati <34954284+nirjalpraj@users.noreply.github.com>
2025-05-28 22:06:47 +0545

    static card layout created

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="./person1.jpg" alt="Placeholder" />
+      <h1>John Doe</h1>
+    </div>
+  );
+};
+
+export default Card;
