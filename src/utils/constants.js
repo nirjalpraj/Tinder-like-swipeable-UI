fbfbee8ae1b1f073658442e74de941e6bdd9e355
Nirjal Prajapati <34954284+nirjalpraj@users.noreply.github.com>
2025-05-29 12:05:06 +0545

    user list created

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+export const users = [
+  {
+    id: 1,
+    name: "Sujan Shrestha",
+    image: "./person2.jpg",
+  },
+  {
+    id: 2,
+    name: "Anita Magar",
+    image: "./person1.jpg",
+  },
+  {
+    id: 3,
+    name: "Dipesh Thapa",
+    image: "./person4.jpg",
+  },
+  {
+    id: 4,
+    name: "Pramila Gurung",
+    image: "./person3.jpg",
+  },
+  {
+    id: 5,
+    name: "Bikash Lama",
+    image: "./person2.jpg",
+  },
+  {
+    id: 6,
+    name: "Sunita Lama",
+    image: "./person1.jpg",
+  },
+  {
+    id: 7,
+    name: "Nabin Tamang",
+    image: "./person4.jpg",
+  },
+  {
+    id: 8,
+    name: "Kabita Rai",
+    image: "./person3.jpg",
+  },
+];
